e922d158bd28786b2e9e8bb29887ba5c02693654
Ng2k03 <73320332+Ng2k03@users.noreply.github.com>
2024-06-14 02:34:25 +0530

    upload css, js, program, jquery, lib, favicon

---
+++ Program.cs
@@ -0,0 +1,34 @@
+using MySqlConnector;
+using WebApplicationn.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add email service to dependency injection to use the service inside of controllers 
+//builder.Services.AddTransient<EmailSender, EmailSender>();
+
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
---
+++ js/site.js
@@ -0,0 +1,5 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+<script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
